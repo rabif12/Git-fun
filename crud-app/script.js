ef7a89556a4f343d0e7804bf89a9c9ee96375c77
Fazle Rabbi <rabif12@aol.com>
2026-09-24 09:47:46 -0400

    feat: completed the form basics this is a great vantigepoint to see it being done from one angle not the best way but it gets the job done next lesson shows better practices

--- crud-app/script.js
+++ crud-app/script.js
@@ -1,14 +1,27 @@
 let form = document.querySelector("#form")
 let input = document.querySelector("#input")
+let blankMsg = document.querySelector(".blank-msg")
+let post1msg = document.querySelector(".post-1-msg")
+let post2msg = document.querySelector(".post-2-msg")
 
 
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
-    console.log("clicked")})
+    console.log("clicked")
+    formValidation()
+})
 
 
 let formValidation = ()=>{
-    if(){}
-    else{}
+    if(input.value === ""){
+        blankMsg.innerHTML="post cannot be blank"
+        console.log("failure type something")
+    }
+    else{
+        post1msg.innerHTML=`${input.value}`
+        console.log("success")
+        input.value = ""
+        blankMsg.innerHTML = ""
+    }
 }
