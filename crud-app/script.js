5c19ddb64a2e9ab66f242a28fbbb32039ad82f9b
FazleRabbi <rabif12@aol.com>
2026-09-24 14:15:51 -0400

    feat: added teh functions to add onto the exisitng posts so you can see a continuous stream of posts

--- crud-app/script.js
+++ crud-app/script.js
@@ -1,6 +1,7 @@
 let form = document.querySelector("#form")
 let input = document.querySelector("#input")
 let blankMsg = document.querySelector(".blank-msg")
+let posts = document.querySelector(".posts")
 let post1msg = document.querySelector(".post-1-msg")
 let post2msg = document.querySelector(".post-2-msg")
 
@@ -19,9 +20,36 @@ let formValidation = ()=>{
         console.log("failure type something")
     }
     else{
-        post1msg.innerHTML=`${input.value}`
+        // post1msg.innerHTML=`${input.value}`
         console.log("success")
+        acceptData()
         input.value = ""
         blankMsg.innerHTML = ""
     }
+}
+
+
+let data = {}
+
+let acceptData = () => {
+    data["text"] = input.value
+    console.log(data) 
+    createPost()
+}
+
+let createPost = () => {
+    posts.innerHTML +=
+    `
+    <div class="post-1 post-list-item">
+        <p class="post-1-msg">${data.text}</p>
+            <span class="post-btns">
+                <p class="edit-o post-btn">
+                    <i class="fa-solid fa-pen-to-square"></i>O
+                </p>
+                <p class="delete-x post-btn">
+                    <i class="fa-solid fa-check"></i>X
+                 </p>
+            </span>
+    </div>
+    `
 }
