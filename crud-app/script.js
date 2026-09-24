efb508f1746332c6672d83e75ff3d9cc26842c77
FazleRabbi <rabif12@aol.com>
2026-09-23 23:03:04 -0400

    completed form submission

---
+++ crud-app/script.js
@@ -0,0 +1,14 @@
+let form = document.querySelector("#form")
+let input = document.querySelector("#input")
+
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    console.log("clicked")})
+
+
+let formValidation = ()=>{
+    if(){}
+    else{}
+}
